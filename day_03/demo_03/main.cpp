09d7245252aa798945d6cb6d1c77cd8f5c9b881b
Xpccccc <1014647664@qq.com>
2023-10-29 20:54:53 +0800

    类的声明和定义分离

--- day_03/demo_03/main.cpp
+++ day_03/demo_03/main.cpp
@@ -1,6 +1,14 @@
 #include <iostream>
+#include "Stack.h"
 
+//类的声明和定义分离
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+    Stack s;
+    s.Init(10);
+    s.Push(1);
+    s.Push(2);
+    s.Push(3);
+    cout << s.Top() << endl;
+    s.Destroy();
     return 0;
 }
