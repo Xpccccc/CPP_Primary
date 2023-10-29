5615edb09a823866a36cc70f063e19a643cbb685
Xpccccc <1014647664@qq.com>
2023-10-30 00:03:11 +0800

    基本类型(内置类型)和自定义类型调用默认构造函数

--- day_04/demo_02/main.cpp
+++ day_04/demo_02/main.cpp
@@ -1,6 +1,36 @@
 #include <iostream>
 
+using namespace std;
+
+class Time {
+public:
+    Time() {
+        cout << "Time()" << endl;
+        _hour = 0;
+        _minute = 0;
+        _second = 0;
+    }
+
+private:
+    int _hour;
+    int _minute;
+    int _second;
+};
+
+
+class Date {
+private:
+    // 基本类型(内置类型)
+    int _year;
+    int _month;
+    int _day;
+    // 自定义类型
+    Time _t;
+};
+
+//基本类型(内置类型)和自定义类型调用默认构造函数
+//基本类型(内置类型)调用自己的默认构造函数，自定义类型调用它的类的默认构造函数（如果没有默认构造函数就需要用到初始化列表）
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+    Date d;
     return 0;
 }
