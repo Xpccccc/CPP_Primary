9cbe5bfad730206e24a81070b9235f4a7bce2858
Xpccccc <1014647664@qq.com>
2023-10-29 22:12:59 +0800

    类对象的存储方式

--- day_03/demo_05/main.cpp
+++ day_03/demo_05/main.cpp
@@ -1,6 +1,29 @@
 #include <iostream>
 
+using namespace std;
+
+// 类中既有成员变量，又有成员函数
+class A1 {
+public:
+    void f1() {}
+
+private:
+    int _a;
+};
+
+// 类中仅有成员函数
+class A2 {
+public:
+    void f2() {}
+};
+
+// 类中什么都没有---空类
+class A3 {
+};
+
+//类对象的存储方式 -- 类的成员变量按结构体的内存对齐存储，类的成员函数存在公共代码区，空类大小为1字节
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+    cout << sizeof(A1) << endl << sizeof(A2) << endl << sizeof(A3) << endl;
     return 0;
 }
+
