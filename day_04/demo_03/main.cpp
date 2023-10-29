9bff34544b8210b15b8c74e1c84d6d591278e78c
Xpccccc <1014647664@qq.com>
2023-10-30 00:03:46 +0800

    缺省成员变量

--- day_04/demo_03/main.cpp
+++ day_04/demo_03/main.cpp
@@ -1,6 +1,36 @@
 #include <iostream>
 
+using namespace std;
+
+class Time {
+public:
+    Time() {
+        cout << "Time()" << endl;
+        _hour = 0;
+        _minute = 0;
+        _second = 0;
+    }
+
+private:
+    int _hour;
+    int _minute;
+    int _second;
+};
+
+class Date {
+private:
+    // 基本类型(内置类型)
+    int _year = 1970;
+    int _month = 1;
+    int _day = 1;
+    // 自定义类型
+    Time _t;
+};
+
+//缺省成员变量
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+    Date d;
     return 0;
 }
+
+
