09b5b457dc8e19f664e02d027d594288e401d8fb
Xpccccc <1014647664@qq.com>
2023-11-27 21:05:52 +0800

    static成员使用规则

---
+++ day_07/demo_16/main.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+
+using namespace std;
+
+//static成员使用规则
+class Date {
+public:
+
+//    Date(int year)
+//            : _year(year) {}
+
+    Date(int year, int month = 1, int day = 1)
+            : _year(year), _month(month), _day(day) {}
+
+
+    //static成员函数没有this指针
+    static void Show(int n) {
+        cout << n << endl;
+    }
+
+private:
+    int _year;
+    int _month;
+    int _day;
+    static int n;//声明
+};
+
+int Date::n = 1;//定义
+
+int main() {
+    Date::Show(2);
+    return 0;
+}
